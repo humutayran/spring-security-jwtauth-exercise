d2cf702bbe9501ddba3eb5a57456db034b4c556f
Hüseyin Umut Reyhanoğlu <h.umutayran@gmail.com>
2024-07-23 16:12:12 +0300

    JwtService

--- src/main/java/com/ayran/security_practice/repositories/UserRepository.java
+++ src/main/java/com/ayran/security_practice/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package com.ayran.security_practice.repositories;
 
-public interface UserRepository {
+import com.ayran.security_practice.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
